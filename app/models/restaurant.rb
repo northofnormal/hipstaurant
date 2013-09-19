1386c1bef946cf4386f7df4510c1442201070dea
Anne <northofnormal@gmail.com>
2013-09-19 17:31:22 -0400

    added associations to user, restaurant, and review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ActiveRecord::Base
+	has_many :diners, through :reservations
+	has_many :owners, through :ownership
+	has_many :reviews
+	has_many :tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :restaurants, through :reservations
+	has_many :reviews
+	has_many :restaurants, through :ownership 
 end
