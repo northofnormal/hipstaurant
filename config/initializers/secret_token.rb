02bd2c633569d062719c20cb312f68a9bc739462
Anne <northofnormal@gmail.com>
2013-09-19 15:37:08 -0400

    Initial Commit, Rspec Installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hipstaurant::Application.config.secret_key_base = 'cb19b3bb824e83eb54e96a30ae9450941561e56fe09f41b6620dcde741a8cef17ffa620fe083b6c65ca401941e352566ffe7a8895ca0988c3a72d23514374d17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hipstaurant::Application.config.session_store :cookie_store, key: '_hipstaurant_session'
